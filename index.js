3112f62fe55c8e17d9b370962b587871aeace86a
tsaniawarda2 <tsaniawarda2@gmail.com>
2025-05-13 01:12:11 +0700

    project

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require('express')
+const express = require("express");
 // import express from 'express'
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Guys!')
-})
+const db = require("./connection");
+
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM produk", (error, result) => {
+    // hasil dari mysql
+    console.log(result);
+    res.status(200).send(result);
+  });
+});
+
+app.get("/find", (req, res) => {
+  const sql = `SELECT * FROM produk WHERE kategori = "${req.query.kategori}"`;
+
+  db.query(sql, (error, result) => {
+    res.json(result);
+  });
+
+  console.log(req.query.kategori);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
